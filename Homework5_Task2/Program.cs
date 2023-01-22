41c710e5513ab8d997deb345a7b39c8ac64be203
Lex <Lexperunovsky@gmail.com>
2023-01-22 19:37:28 +0300

    Homework6_Task1 is complete

--- Homework5_Task2/Program.cs
+++ Homework5_Task2/Program.cs
@@ -2,10 +2,10 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
  */
-int[] array = GetRandomArray(5, 0, 100);
+int[] array = GetRandomArray(6, 0, 100);
 Console.WriteLine($"[{String.Join(", ", array)}]");
-SumOddNumbers(array);
-Console.WriteLine($"Сумма нечетных чисел массива = {array}");
+int oddNumber = SumOddNumbers(array);
+Console.WriteLine($"Сумма нечетных чисел массива = {oddNumber}");
 
 int[] GetRandomArray(int size, int minValue, int maxValue)
 {
@@ -24,7 +24,7 @@ int SumOddNumbers(int[] array)
     int sumOdd = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (i % 2 == 1) sumOdd += array[i];
+        if (i % 2 != 0) sumOdd += array[i];
     }
     return sumOdd;
 }
---
+++ Homework6_Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+   
+          
+    Console.Write($"Введите количество элементов массива : ");
+    int elementsCount = int.Parse(Console.ReadLine());
+    int []array = new int[elementsCount];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\n ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    
+
+    Console.Write("Вывод Массива");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    } 
+
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+            Console.WriteLine($"Количество положительных чисел введенных пользователем = {count}");
+        }
+                
+        
+    }
+  
+
+
+
+/* int CheckPositiveNumbers(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) i++;
+    }
+    return array;
+} */
