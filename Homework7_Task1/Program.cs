a0e958d05836bafc308e51358b848445c23807a5
Lex <Lexperunovsky@gmail.com>
2023-01-26 16:56:33 +0300

    Homework7_Task1 is complete

---
+++ Homework7_Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.Clear();
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+
+double[,] array = GetDoubleArray(rows, columns);
+PrintArray(array);
+
+
+/////////////////////////////////////////////////////////////////
+
+double[,] GetDoubleArray(int m, int n)
+{
+    Random rnd = new Random();
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = (double)rnd.Next(-100, 100) / 10;
+        }
+    }
+    return result;
+}
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine(" ");
+    }
+}
