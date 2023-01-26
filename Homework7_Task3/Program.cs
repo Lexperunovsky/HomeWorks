16d7d933561d30554baac1fe62a711b193ba935c
Lex <Lexperunovsky@gmail.com>
2023-01-26 19:05:28 +0300

    I have a trouble

---
+++ Homework7_Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+ */
+Console.Clear();
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+ 
+ int [,] array = new int[rows,columns];
+ 
+ Random rnd = new Random();
+ 
+  int [,]numbers = new int[rows,columns];
+ 
+for (int i = 0; i < rows; ++i)
+{
+    for (int j = 0; j < columns; j++)
+    {
+       numbers[i,j] = rnd.Next(0,10);
+        
+    }
+       
+}
+ 
+
